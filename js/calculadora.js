e63d73be7aa9991b6f314eccfcb7d10803800586
Maicon Machado <147193871+maiconarroyo@users.noreply.github.com>
2024-10-31 22:32:56 -0300

    js da calculadora

---
+++ js/calculadora.js
@@ -0,0 +1,47 @@
+function hidem() {
+    const botomCalculator = document.querySelector('.icon-calculator');
+    const calcularDiv = document.querySelector('.calcular-amonia');
+    calcularDiv.style.display = 'flex';
+    botomCalculator.style.display = 'none';
+}
+
+function amonia() {
+    const amoniaInput = document.querySelector('input[name="amonia"]');
+    const phInput = document.querySelector('input[name="ph"]');
+    const temperaturaInput = document.querySelector('input[name="temperatura"]');
+
+    let amoniaTotal = parseFloat(amoniaInput.value);
+    let ph = parseFloat(phInput.value);
+    let temperatura = parseFloat(temperaturaInput.value);
+
+    if (isNaN(temperatura) || isNaN(ph) || isNaN(amoniaTotal)) {
+        alert("Por favor, insira valores válidos.");
+        return;
+    }
+
+    function calcularPK(temperatura) {
+        return 0.09018 + (2729.92 / (273.15 + temperatura));
+    }
+
+    const pK = calcularPK(temperatura);
+    const fracaoToxica = 1 / (1 + Math.pow(10, (pK - ph)));
+    const amoniaToxica = amoniaTotal * fracaoToxica;
+
+    const resultadoDiv = document.getElementById('resultado');
+    const resultadoTexto = resultadoDiv.querySelector('.sub-title.-resultado');
+    resultadoDiv.style.display = 'block';
+
+    if (amoniaToxica <= 0.018) {
+        resultadoDiv.style.backgroundColor = 'green';
+        resultadoTexto.style.color = 'white';
+        resultadoTexto.textContent = "Níveis de Amônia Seguros";
+    } else if (amoniaToxica <= 0.039) {
+        resultadoDiv.style.backgroundColor = 'yellow';
+        resultadoTexto.style.color = 'black';
+        resultadoTexto.textContent = "Cuidado! Níveis de Amônia Moderado";
+    } else {
+        resultadoDiv.style.backgroundColor = 'red';
+        resultadoTexto.style.color = 'white';
+        resultadoTexto.textContent = "Níveis de Amônia Críticos!!";
+    }
+}
