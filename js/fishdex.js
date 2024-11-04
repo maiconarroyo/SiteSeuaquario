627bc4a3dfe4e8a0573beb72480d1b1fccfebfcd
Maicon Machado <147193871+maiconarroyo@users.noreply.github.com>
2024-11-04 14:46:08 -0300

    fish dex js corrigido

--- js/fishdex.js
+++ js/fishdex.js
@@ -1,11 +1,9 @@
 function fishDex(){
-    const paragrafo = document.querySelector(".amonia-introduce");
+    const paragrafo = document.querySelector(".main-section.-calculator");
     const background = document.querySelector(".main-background.-amonia");
-    // Oculta o background e o parágrafo
-    if (background) {
-        background.style.display = "none"; // Oculta o background
-    }
-    if (paragrafo) {
-        paragrafo.style.display = "none"; // Oculta a introdução
-    }
+    const cardDiv = document.querySelector(".main-section.-fishdex");
+
+    paragrafo.style.display= 'none';
+    background.style.display= 'none';
+    cardDiv.style.display='flex';
 }
