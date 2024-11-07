9967d618308f1e42110d8d4174833c5e490fd092
Maicon Machado <147193871+maiconarroyo@users.noreply.github.com>
2024-11-07 19:56:14 -0300

    adicionado no carrosel transição de titulo e subtitulo

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,6 +1,17 @@
 const imgs = document.getElementById("img");
 const img = document.querySelectorAll("#img img");
+const titleElement = document.querySelector(".main-title.-h1Carrosel")
+const suubTitleElement = document.querySelector(".main-subtitle.-pCarrosel")
 
+const contents = [
+    { title: 'Início 2017', subtitle: 'Tudo começou em 2017 quando, ao enfrentar dificuldades em cuidar de um peixe betta, decidi criar um canal para ajudar pessoas com dificuldades no Aquarismo.' },
+    { title: 'Trabalho em Família', subtitle: 'Mesmo sem nenhuma experiência em criação de conteúdo, edição de vídeo ou qualquer outra área, comecei com a ajuda da minha esposa, que me apoia na criação de conteúdo para o canal.' },
+    { title: 'Canal começa a Crescer', subtitle: 'Apesar da concorrência de muitos canais já estabelecidos e reconhecidos, o nosso canal começou a crescer e aumentar o número de inscritos, superando desafios e expectativas. Decidimos diversificar os temas dos vídeos e, mesmo assim, conseguimos manter um crescimento constante.' },
+    { title: 'Uma empresa que acreditou no nosso Sonho', subtitle: 'Decidimos enviar um e-mail a uma grande empresa, mesmo sendo um canal pequeno, solicitando seu apoio. Para nossa surpresa, essa iniciativa gerou uma amizade e um suporte que fizeram toda a diferença. A MBreda, acreditando em nosso projeto, nos proporcionou a oportunidade de criar ainda mais conteúdo com seu apoio.' },
+    { title: 'Nosso Primeiro Evento', subtitle: 'Com o apoio de algumas empresas, decidimos realizar nosso primeiro evento com o objetivo de divulgar o hobby e apoiar nossos colegas aquaristas.' },
+    { title: 'Ganhamos nossa Marca', subtitle: 'Com o apoio do nosso amigo Diego, recebemos uma arte que se tornou nossa marca registrada desde então.' },
+    { title: 'Criamos nosso Curso', subtitle: 'Com o intuito de compartilhar conhecimento com todos, criamos nosso curso de aquarismo a um preço extremamente acessível, oferecendo material de alta qualidade. Com a receita gerada pelo curso, iniciamos o desenvolvimento do nosso site e a produção de novos vídeos.' }
+];
 let idx =0;
 
 function carossel(){
@@ -10,5 +21,8 @@ function carossel(){
     }
 
     imgs.style.transform = `translateX(${-idx * 375}px)`;
+
+    titleElement.textContent = contents[idx].title;
+    suubTitleElement.textContent = contents[idx].subtitle;
 }
 setInterval(carossel, 1800)
